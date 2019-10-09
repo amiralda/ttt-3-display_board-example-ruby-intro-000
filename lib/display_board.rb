e17ea8d314308719648384eca0edb6cbf45c211f
Dany AUGUSTIN <augustindany@hotmail.com>
2019-10-09 18:59:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
  def display_board
-
- end
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+ end 
  display_board
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
